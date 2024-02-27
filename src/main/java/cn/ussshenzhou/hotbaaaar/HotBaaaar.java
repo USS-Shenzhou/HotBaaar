1edc65bcf92630b35031a202a6aef53fd08c6e0e
USS-Shenzhou <969840795@qq.com>
2024-02-27 20:33:05 +0800

    init

---
+++ src/main/java/cn/ussshenzhou/hotbaaaar/HotBaaaar.java
@@ -0,0 +1,19 @@
+package cn.ussshenzhou.hotbaaaar;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+/**
+ * @author USS_Shenzhou
+ */
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(HotBaaaar.MOD_ID)
+public class HotBaaaar
+{
+    public static final String MOD_ID = "hotbaaaar";
+
+    public HotBaaaar(IEventBus modEventBus)
+    {
+
+    }
+}
