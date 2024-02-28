893d021be3e3b521037dc52427357fe9a680598f
USS-Shenzhou <969840795@qq.com>
2024-02-28 09:46:01 +0800

    forgix

---
+++ fabric/src/main/java/cn/ussshenzhou/hotbaaaar/HotBaaaar.java
@@ -0,0 +1,13 @@
+package cn.ussshenzhou.hotbaaaar;
+
+import net.fabricmc.api.ModInitializer;
+
+/**
+ * @author USS_Shenzhou
+ */
+public class HotBaaaar implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ fabric/src/main/java/cn/ussshenzhou/hotbaaaar/client/HotBaaaarClient.java
@@ -0,0 +1,13 @@
+package cn.ussshenzhou.hotbaaaar.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+/**
+ * @author USS_Shenzhou
+ */
+public class HotBaaaarClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
